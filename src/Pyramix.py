603e0f044620ba5de12a98e841d24d9a982088f0
r1407p <buszter0403@gmail.com>
2024-11-12 08:26:54 +0000

    feat: display the Pyramimx

--- src/Pyramix.py
+++ src/Pyramix.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class Pyraminx(object):
     def __init__(self):
         """
@@ -14,10 +17,45 @@ class Pyraminx(object):
             "D": [["B"], ["B", "B", "B"], ["B", "B", "B", "B", "B"]],
         }
 
+    def display(self):
+        """
+        Display the Pyraminx in a triangular format similar to the provided image.
+        """
+
+        def format_layer(layer):
+            return " ".join(layer).center(11)
+
+        print(
+            "\n"
+            + format_layer(self.faces["L"][0])
+            + "  "
+            + format_layer(self.faces["F"][0])
+            + "  "
+            + format_layer(self.faces["R"][0])
+        )
+        print(
+            format_layer(self.faces["L"][1])
+            + "  "
+            + format_layer(self.faces["F"][1])
+            + "  "
+            + format_layer(self.faces["R"][1])
+        )
+        print(
+            format_layer(self.faces["L"][2])
+            + "  "
+            + format_layer(self.faces["F"][2])
+            + "  "
+            + format_layer(self.faces["R"][2])
+        )
+        print(" " * 13 + format_layer(self.faces["D"][2]))
+        print(" " * 13 + format_layer(self.faces["D"][1]))
+        print(" " * 13 + format_layer(self.faces["D"][0]))
+
 
 def main():
     pyramix = Pyraminx()
-    print(pyramix.faces)
+    pprint(pyramix.faces)
+    pyramix.display()
 
 
 if __name__ == "__main__":
