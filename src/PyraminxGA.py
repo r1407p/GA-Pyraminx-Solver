b71f969b7184bf3f4532aa363b62f9c24c198276
110550035 <kono.c@nycu.edu.tw>
2024-11-29 20:37:57 +0800

    feat: miscellaneous

--- src/PyraminxGA.py
+++ src/PyraminxGA.py
@@ -1,17 +1,23 @@
 import os
 from collections import namedtuple
+from enum import Enum
 
 from pygad import GA
 from Pyraminx import Pyraminx
 
 
 class PyraminxGA:
+    class Mode(Enum):
+        EXPERT = 0
+        FROM_SCRATCH = 1
+
     NUM_STAGES = 4
-    GENE_SPACES = (["r", "l", "b", "u", "r'", "l'", "b'", "u'"],
+    GENE_SPACES = (["r", "l", "b", "u", "r'", "l'", "b'", "u'", "R", "L", "B", "U", "R'", "L'", "B'", "U'"],
+                   ["r", "l", "b", "u", "r'", "l'", "b'", "u'"],
                    ["R", "L", "B", "U", "R'", "L'", "B'", "U'"],
                    ["E", "F", "G", "H", "I", "J", "U", "U'"],
                    ["X", "Y", "Z", "U", "U'"])
-    TARGET = (4, 4, 3, 4)
+    TARGET = (0, 4, 4, 3, 4)
 
     StageResult = namedtuple("StageResult", ["solution", "fitness", "generation", "is_solved"])
 
@@ -19,8 +25,8 @@ class PyraminxGA:
         self.pyraminx = Pyraminx()
         self.pyraminx.shuffle()
 
-        self.num_genes = (8, 8, 20, 12)
-        self.min_fitness_solved = tuple(target - num_genes for target, num_genes in zip(PyraminxGA.TARGET, self.num_genes))
+        self.num_genes = (8, 8, 32, 12)
+        self.min_fitness_solved = tuple(target - num_genes * 0.01 for target, num_genes in zip(PyraminxGA.TARGET[1:], self.num_genes))
 
         self.results = []
 
@@ -31,11 +37,11 @@ class PyraminxGA:
 
     @staticmethod
     def _is_solved_target(stage: int, target: int):
-        return target == PyraminxGA.TARGET[stage - 1]
+        return target == PyraminxGA.TARGET[stage]
 
     @staticmethod
     def _gene2move(stage: int, move: int):
-        return PyraminxGA.GENE_SPACES[stage - 1][move]
+        return PyraminxGA.GENE_SPACES[stage][move]
 
     @staticmethod
     def _genes2moves(stage: int, moves: list[int]):
@@ -46,6 +52,9 @@ class PyraminxGA:
         pyraminx = pyraminx.copy()
 
         match stage:
+            case 0:
+                def target_function():
+                    return pyraminx.small_corners_solved() + pyraminx.large_corners_solved() + pyraminx.middle_pieces_solved() + sum(pyraminx.num_colors_on_a_face())
             case 1:
                 target_function = pyraminx.small_corners_solved
             case 2:
@@ -53,7 +62,8 @@ class PyraminxGA:
             case 3:
                 target_function = pyraminx.middle_pieces_solved
             case 4:
-                def target_function(): return sum(pyraminx.num_colors_on_a_face())
+                def target_function():
+                    return sum(pyraminx.num_colors_on_a_face())
 
         steps = 0
         target = target_function()
@@ -70,7 +80,7 @@ class PyraminxGA:
     def _fitness(pyraminx: Pyraminx, stage: int):
         def fitness(ga, solution, index):
             target, steps = PyraminxGA._best_target(pyraminx, stage, solution)
-            return target - steps
+            return target - steps * 0.01
         return fitness
 
     @staticmethod
@@ -90,21 +100,21 @@ class PyraminxGA:
         ga.run()
         ga.plot_fitness(save_dir=f"{save_dir}/fitness_stage{stage}.png")
         solution, fitness, _ = ga.best_solution()
-        return PyraminxGA.StageResult(solution, fitness, ga.best_solution_generation, self._is_solved_fitness(stage, fitness=fitness))
+        return PyraminxGA.StageResult(solution, fitness, ga.best_solution_generation, self._is_solved_fitness(stage, fitness=fitness))  # type: ignore
 
-    def run(self, **kwargs):
+    def run(self, mode: Mode = Mode.EXPERT, **kwargs):
         common_kwargs = {
-            "num_generations": 100,  # [100, 1000]
+            "num_generations": 10000,  # [100, 1000]
             "num_parents_mating": 2,  # [2, 5]
-            "sol_per_pop": 100,  # [100, 1000]
+            "sol_per_pop": 2,  # [100, 1000]
             "gene_type": int,
             "parent_selection_type": "tournament",
-            "keep_elitism": 100,
+            "keep_elitism": 2,
             "crossover_type": "single_point",
             "crossover_probability": 0.7,  # [0.5, 0.9]
             "mutation_type": "random",  # only one gene
-            "mutation_probability": 0.05,  # [0.01, 0.1]
-            "mutation_by_replacement": False,
+            "mutation_probability": 0.1,  # [0.01, 0.1]
+            "mutation_by_replacement": True,
             "mutation_percent_genes": 'default',  # based on population size and the number of generations
             "mutation_num_genes": None,  # number of genes in each solution will be randomly selected for mutation, covering mutation_percent_genes
             "on_start": None,
@@ -115,23 +125,38 @@ class PyraminxGA:
             "on_generation": None
         }
         stage_kwargs = tuple({"num_genes": num_genes, "gene_space": range(len(gene_space))}
-                             for num_genes, gene_space in zip(self.num_genes, PyraminxGA.GENE_SPACES))
+                             for num_genes, gene_space in zip(self.num_genes, PyraminxGA.GENE_SPACES[1:]))
 
         pyraminx = self.pyraminx.copy()
-        for stage in range(1, 5):
-            self.results.append(self._run_stage(stage, fitness_func=PyraminxGA._fitness(pyraminx, stage), **(common_kwargs | stage_kwargs[stage - 1] | kwargs)))
-            if not self.results[-1].is_solved:
-                break
-            PyraminxGA._apply_valid_moves(pyraminx, stage, self.results[-1].solution)
+        self.results.clear()
+
+        self.mode = mode
+        match mode:
+            case PyraminxGA.Mode.FROM_SCRATCH:
+                stage_kwargs = {"num_genes": 36, "gene_space": range(len(PyraminxGA.GENE_SPACES[0]))}
+                self.results.append(self._run_stage(0, fitness_func=PyraminxGA._fitness(pyraminx, 0), **(common_kwargs | stage_kwargs | kwargs)))
+            case PyraminxGA.Mode.EXPERT:
+                pyraminx.display()
+                for stage in range(1, 5):
+                    self.results.append(self._run_stage(stage, fitness_func=PyraminxGA._fitness(pyraminx, stage), **(common_kwargs | stage_kwargs[stage - 1] | kwargs)))
+                    print(f"{self.results[-1].fitness=}")
+                    if not self.results[-1].is_solved:
+                        break
+                    PyraminxGA._apply_valid_moves(pyraminx, stage, self.results[-1].solution)
+                    pyraminx.display()
 
         return self.results[-1].is_solved
 
     def best_solution(self):
         pyraminx = self.pyraminx.copy()
         aggregated_solution = []
-        for stage, result in enumerate(self.results, start=1):
-            valid_moves = PyraminxGA._apply_valid_moves(pyraminx, stage, result.solution)
-            aggregated_solution.extend(valid_moves)
+        match self.mode:
+            case PyraminxGA.Mode.FROM_SCRATCH:
+                aggregated_solution = PyraminxGA._apply_valid_moves(pyraminx, 0, self.results[-1].solution)
+            case PyraminxGA.Mode.EXPERT:
+                for stage, result in enumerate(self.results, start=1):
+                    valid_moves = PyraminxGA._apply_valid_moves(pyraminx, stage, result.solution)
+                    aggregated_solution.extend(valid_moves)
         return aggregated_solution
 
     def best_solution_generation(self):
